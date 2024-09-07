6475b4c1decf73fa605cec751f4d5b6492c62dbb
fayiz-towner <fayiz@taxiondc.com>
2024-09-07 10:09:23 +0530

    updated

--- flood/urls.py
+++ flood/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# latest update
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import (
